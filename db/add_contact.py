f1d438716fc0dbac271f1afd67baf857f35ae65d
Adel Suarez <adel90suarez@gmail.com>
2022-02-28 00:22:23 -0400

    caries name change

--- db/add_contact.py
+++ db/add_contact.py
@@ -1,27 +1,30 @@
-from db.models import contact_list
+from db.models import Contact_list
 from db.Database import session
 import tkinter as tk
 import tkinter.messagebox
 from db.get_all_contacts import Get_all_contacts
 
 class Add_contact:
-    def __init__(self, name, phone, email, name_label, phone_label, tree):
-        self._name = name
-        self._phone = phone
-        self._email = email
+    def __init__(self, name_entry, phone_entry, email_entry, name_label, phone_label, tree):
+        self._name_entry = name_entry
+        self._phone_entry = phone_entry
+        self._email_entry = email_entry
         self.name_label = name_label
         self.phone_label = phone_label
         self._tree = tree
         self.add()
 
     def add(self):
-        if self.validate_empty_fields(self._name.get()):
+        name_entry_get = self._name_entry.get()
+        phone_entry_get = self._phone_entry.get()
+
+        if self.validate_empty_fields(name_entry_get):
             self.name_label['fg'] = '#000'
 
-            if self.validate_empty_fields(self._phone.get()) and self.validate_empty_int():
+            if self.validate_empty_fields(phone_entry_get) and self.validate_empty_int(phone_entry_get):
                 self.phone_label['fg'] = '#000'
 
-                contact = contact_list(self._name.get(), self._phone.get(), self._email.get())
+                contact = Contact_list(name_entry_get, phone_entry_get, self._email_entry.get())
                 session.add(contact)
                 print(repr(contact))
                 session.commit()
@@ -38,17 +41,18 @@ class Add_contact:
             self.name_label['fg'] = 'red'
             
 
-    def validate_empty_fields(self, input):
-        return len(input) != 0
+    def validate_empty_fields(self, entry):
+        return len(entry) != 0
 
     def clear_inputs(self):
-        self._name.delete(0, tk.END)
-        self._phone.delete(0, tk.END)
-        self._email.delete(0, tk.END)
+        self._name_entry.delete(0, tk.END)
+        self._phone_entry.delete(0, tk.END)
+        self._email_entry.delete(0, tk.END)
 
-    def validate_empty_int(self):
+    def validate_empty_int(self, phone):
+        # Verify that the phone entered is numbers
         try:
-            if int(self._phone.get()):
+            if int(phone):
                 value = True
         except:
             value = False

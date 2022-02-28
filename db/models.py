ea7bea5e5a7083a34d700d2e13b1bef9888c7052
Adel Suarez <adel90suarez@gmail.com>
2022-02-28 00:24:58 -0400

    Delete import

--- db/models.py
+++ db/models.py
@@ -1,7 +1,7 @@
-from db.Database import Base, engine
+from db.Database import Base
 from sqlalchemy import create_engine, Column, Integer, String
 
-class contact_list(Base):
+class Contact_list(Base):
     __tablename__ = 'contacts'
     contact_id = Column(Integer, primary_key=True)
     contact_name = Column(String(50))
@@ -15,7 +15,7 @@ class contact_list(Base):
         self.contact_email = email
 
     def __repr__(self) -> str:
-        return f'Contacto:\nNombre: {self.contact_name}.\nNúmero: {self.contact_phone}.\nCorreo: {self.contact_email}\n'
+        return f'Contact:\nName: {self.contact_name}.\nPhone: {self.contact_phone}.\nEmail: {self.contact_email}\n'
 
     def __str__(self) -> str:
         return self.contact_name
